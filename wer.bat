1a011350f5676639bc4b1fb318169f5eb3650d9e
HazmatDemon <michael0march@gmail.com>
2020-07-05 07:12:54 +0000

    Deleted ler and letd files.

--- wer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cmd /c "scons p=windows bits=64 target=release_debug tools=yes -j12"
-
-PAUSE
--- wetr.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cmd /c "scons p=windows bits=64 tools=no target=release -j12"
-PAUSE
