09f423405499ea238875a25e568906aec64bc3d6
HazmatDemon <michael0march@gmail.com>
2020-07-21 17:36:05 +0200

    Some generic code touch ups

--- modules/discord_game_sdk/discord_game_sdk.h
+++ modules/discord_game_sdk/discord_game_sdk.h
@@ -31,16 +31,16 @@
 #ifndef DISCORD_GAME_SDK_H
 #define DISCORD_GAME_SDK_H
 
-#include "scene/main/node.h"
 #include "core/class_db.h"
 #include "core/error_macros.h"
+#include "scene/main/node.h"
 
 #include <core.h>
 #include <ffi.h>
 
 class DiscordGameSDK : public Node {
 
-    GDCLASS(DiscordGameSDK, Node);
+	GDCLASS(DiscordGameSDK, Node);
 
 public:
 	enum Result {
@@ -60,7 +60,7 @@ public:
 		NO_ELIGIBLE_ACTIVITY = DiscordResult_NoEligibleActivity,
 		INVALID_INVITE = DiscordResult_InvalidInvite,
 		NOT_AUTHENTICATED = DiscordResult_NotAuthenticated,
-		INVALIS_ACCESS_TOKEN = DiscordResult_InvalidAccessToken,
+		INVALID_ACCESS_TOKEN = DiscordResult_InvalidAccessToken,
 		APPLICATION_MISMATCH = DiscordResult_ApplicationMismatch,
 		INVALID_DATA_URL = DiscordResult_InvalidDataUrl,
 		INVALID_BASE_64 = DiscordResult_InvalidBase64,
@@ -80,7 +80,7 @@ public:
 		INAVLID_ORIGIN = DiscordResult_InvalidOrigin,
 		RATE_LIMIT = DiscordResult_RateLimited,
 		OAUTH2_ERROR = DiscordResult_OAuth2Error,
-		SELCT_CHANNEL_TIMEOUT = DiscordResult_SelectChannelTimeout,
+		SELECT_CHANNEL_TIMEOUT = DiscordResult_SelectChannelTimeout,
 		GET_GUILD_TIMEOUT = DiscordResult_GetGuildTimeout,
 		SELECT_VOICE_FORCE_REQUIRED = DiscordResult_SelectVoiceForceRequired,
 		CAPTURE_SHORTCUT_ALREADY_LISTENING = DiscordResult_CaptureShortcutAlreadyListening,
@@ -124,40 +124,49 @@ private:
 
 	discord::Activity activity{};
 
-	void _lobby_created(Result result, int64_t lobby_id, int64_t owner_id, const String& secret, bool is_locked);
-	void _lobbies_found(Result result, int32_t count);
-	void _lobby_connected(Result result);
-	void _voice_connected(Result result);
+	void _activity_updated(Result result);
+	void _lobby_created(Result result, int64_t lobby_id, int64_t owner_id, const String &secret, bool is_locked);
 
+	void _lobby_type_updated(Result result, int64_t lobby_id);
+	void _lobby_get_type(Result result, int64_t lobby_id, LobbyType lobby_type);
+
+	void _connected_to_lobby(Result result);
+	void _connected_to_lobby_voice(Result result);
+	
 protected:
+	static void _bind_methods();
 	void _notification(int p_what);
-    static void _bind_methods();
 
 public:
+	// Core
 	Result create(int64_t client_id, CreateFlags flag);
 
-	Result update_activity() const;
+	void update_activity();
 
-    void set_time_left(int64_t stamp);
-	int64_t get_time_left() const;
+	// Activity
+	void set_activity_time_left(int64_t stamp);
+	int64_t get_activity_time_left() const;
 
-	void set_state(const String &state);
-	String get_state() const;
+	void set_activity_state(const String &state);
+	String get_activity_state() const;
 
-    void set_details(const String& detail);
-	String get_details() const;
+	void set_activity_details(const String &detail);
+	String get_activity_details() const;
 
-	void set_large_image(const String& image);
-	String get_large_image() const;
-	
-	void create_lobby(const String& name, int capacity, LobbyType type);
+	void set_activity_large_image(const String &image);
+	String get_activity_large_image() const;
 
-	void find_lobbies(const String& key, LobbySearchComparison comp, LobbySearchCast cast, const String& value, uint32_t count);
-	void connect_lobby(int64_t lobby_id, const String& secret);
-	void connect_voice(int64_t lobby_id);
+	// Lobby
+	void create_lobby(const String &name, uint32_t capacity, LobbyType type);
 
-    DiscordGameSDK();
-    ~DiscordGameSDK();
+	void set_lobby_type(int64_t lobby_id, LobbyType lobby_type);
+	void get_lobby_type(int64_t lobby_id);
+
+	void connect_to_lobby(int64_t lobby_id, const String &secret);
+	void connect_to_lobby_voice(int64_t lobby_id);
+
+	DiscordGameSDK();
+	~DiscordGameSDK();
 };
 
 VARIANT_ENUM_CAST(DiscordGameSDK::Result);
